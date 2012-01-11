63865f33bd3d43857c665e4d8bc9e6e31338731e
Dmitry Veremchuk <dmitry.veremchuk@gmail.com>
2012-01-11 07:37:10 +0300

    first commit

---
+++ shippie/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shippie::Application.config.secret_token = 'ec5521adb2b80a5c24d33bf31c77471366b98fca58fb63baea0fd7b72ea3242e038ca6e03ccc8849186e539394efd952689c7c88ac95907c3a0baa8b5406ad44'
